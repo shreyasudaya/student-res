0b220cf67ae1f0f7affde2d76e119ebee34b3392
Shreyas Udaya <shreyasudaya03@gmail.com>
2023-06-24 09:34:19 +0530

    frontend templates

---
+++ student_resource_sharing/studentres/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    return render(request,'about.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def login_admin(request) :
+    return render(request, 'login_admin.html')
+
+def signup(request) :
+    return render(request, 'signup.html')
